260855406eb2d2456a22bbaeb6cfcc0ba4f10065
James First <jamesfirstv@gmail.com>
2021-07-22 01:52:15 +0200

    Тест Telegraf

--- main.js
+++ main.js
@@ -1,40 +1,11 @@
-import { Telegraf } from 'vendor/telegraf'
 const BOT_TOKEN = ''
+const { Telegraf } = require('telegraf')
+
 const bot = new Telegraf(BOT_TOKEN)
-
-bot.command('quit', (ctx) => {
-  // Explicit usage
-  ctx.telegram.leaveChat(ctx.message.chat.id)
-
-  // Using context shortcut
-  ctx.leaveChat()
-})
-
-bot.on('text', (ctx) => {
-  // Explicit usage
-  ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
-
-  // Using context shortcut
-  ctx.reply(`Hello ${ctx.state.role}`)
-})
-
-bot.on('callback_query', (ctx) => {
-  // Explicit usage
-  ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
-
-  // Using context shortcut
-  ctx.answerCbQuery()
-})
-
-bot.on('inline_query', (ctx) => {
-  const result = []
-  // Explicit usage
-  ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
-
-  // Using context shortcut
-  ctx.answerInlineQuery(result)
-})
-
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
 
 // Enable graceful stop
